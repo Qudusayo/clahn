8510a8baed80f2c5fe429fad1d198a321d12a03c
Ayoola Abdulqudus <qqudusayo@gmail.com>
2024-02-26 12:39:55 +0100

    Update button styling to use rounded-2xl

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const Button = (
 
   return (
     <button
-      className={`text-white bg-[#F36F00] hover:bg-[#F36F00] focus:outline-none focus:ring-4 focus:ring-[#F36F00] font-medium rounded-full text-sm px-5 py-2.5 text-cente dark:bg-[#F36F00] dark:hover:bg-[#F36F00] dark:focus:ring-[#F36F00] ${className}`}
+      className={`text-white bg-[#F36F00] hover:bg-[#F36F00] focus:outline-none focus:ring-4 focus:ring-[#F36F00] font-medium rounded-2xl text-sm px-5 py-2.5 text-cente dark:bg-[#F36F00] dark:hover:bg-[#F36F00] dark:focus:ring-[#F36F00] ${className}`}
       {...rest}
     >
       {props.children}
